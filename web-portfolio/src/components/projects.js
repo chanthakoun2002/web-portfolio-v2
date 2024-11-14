37223190b7c6b6059491aa16551c9a7c1b52c4d3
chanthakoun2002 <chanthakounandrew@gmail.com>
2024-11-13 16:50:41 -0800

    added links and images
    improved links in project overlays and added more images for projects

--- web-portfolio/src/components/projects.js
+++ web-portfolio/src/components/projects.js
@@ -28,8 +28,8 @@ const projectData = [
     images: [portfolioImg1, portfolioImg2],
     description: 'My humble portfolio showcasing information and background about me, my skills and projects.',
     techDescription: 'HTML/CSS | javascript | React.js',
-    githubLink: 'https://github.com/chanthakoun2002/web-portfolio',
-    siteLink: 'https://github.com/chanthakoun2002/web-portfolio'
+    githubLink: 'https://github.com/chanthakoun2002/web-portfolio-v2',
+    siteLink: 'https://andrewchanthakoun.com/'
   },
   {
     title: 'Inventory Management ',
@@ -38,7 +38,7 @@ const projectData = [
     description: 'An inventory management web application with CRUD operations to handle user login/registration and authentication. Allows users to alter inventory items and create orders that update the data in a noSQL database.',
     techDescription: 'HTML/CSS | javascript | Express.js | MongoDB',
     githubLink: 'https://github.com/chanthakoun2002/Inventory-management-api',
-    siteLink: 'https://github.com/chanthakoun2002/web-portfolio'
+    siteLink: 'https://github.com/chanthakoun2002/Inventory-management-api'
   },
   {
     title: 'Sword Adventurer',
@@ -48,7 +48,7 @@ const projectData = [
     description: 'An Isometric 2D action fighter game where you go around and try to slay as many slimes as you can before you die, created using C# in the unity engine.',
     techDescription: 'C# | Unity Engine',
     githubLink: 'https://github.com/chanthakoun2002/game-dev-project-2d-isometric-rpg',
-    siteLink: 'https://github.com/chanthakoun2002/web-portfolio'
+    siteLink: 'https://thermalsiren2002.itch.io/sword-adventurer'
   },
   
   
